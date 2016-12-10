d5578fec9723202a0aff5a726e362a6e5d430f3a
Harrison Hocker <harrisonhocker@Harrisons-MBP.att.net>
2016-12-09 19:26:16 -0600

    decision tree with anaconda

---
+++ decision_tree/decision tree.py
@@ -0,0 +1,46 @@
+from sklearn import tree
+
+features = [[140, 1], [130, 1], [150,0], [170,0]]
+labels = [0,0,1,1]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+print clf.predict([[150,0]])
+
+
+
+from sklearn.datasets import load_iris
+import numpy as np
+
+iris = load_iris()
+test_idx = [0,50,100]
+
+#training data
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx, axis=0)
+
+#test data
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data, train_target)
+
+print clf.predict(test_data)
+print test_target
+
+#Visualize the decision tree
+from sklearn.externals.six import StringIO
+import pydotplus
+
+dot_data = StringIO()
+tree.export_graphviz(clf, 
+                                out_file=dot_data, 
+                                feature_names=iris.feature_names,  
+                                class_names=iris.target_names,  
+                                filled=True, 
+                                rounded=True,  
+                                special_characters=False)  
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_pdf('decision_tree.pdf')
